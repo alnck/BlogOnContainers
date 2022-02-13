64acd17e846c527d90665a07cb55b16072fd29c6
Eren <alincakeren@gmail.com>
2022-02-13 19:23:21 +0300

    GetRequest Removed from authorization

--- src/services/blog-api/api.go
+++ src/services/blog-api/api.go
@@ -12,29 +12,29 @@ func main() {
 	r.POST("/login", handler.LoginHandler)
 
 	api := r.Group("/api")
-	api.Use(middleware.ValidateToken())
 
-	initBlogRouteMap(api)
-	initUserRouteMap(api)
+	validateTokenHandle := middleware.ValidateToken()
+	authHandle := middleware.Authorization([]int{1})
+
+	initBlogRouteMap(api, validateTokenHandle, authHandle)
+	initUserRouteMap(api, validateTokenHandle, authHandle)
 
 	r.Run(":5000")
 }
 
-func initBlogRouteMap(route *gin.RouterGroup) {
+func initBlogRouteMap(route *gin.RouterGroup, validateTokenHandle, authHandle gin.HandlerFunc) {
 	blog := route.Group("/blog")
-	blog.Use(middleware.Authorization([]int{1}))
 
-	blog.POST("/", handler.CreateStory)
-	blog.POST("/:id", handler.UpdateStory)
+	blog.POST("/", handler.CreateStory, validateTokenHandle, authHandle)
+	blog.POST("/:id", handler.UpdateStory, validateTokenHandle, authHandle)
+	blog.DELETE("/:id", handler.DeleteStory, validateTokenHandle, authHandle)
 
 	blog.GET("/", handler.GetStories)
-
-	blog.DELETE("/:id", handler.DeleteStory)
-
 }
 
-func initUserRouteMap(route *gin.RouterGroup) {
-	user := route.Group("/user")
+func initUserRouteMap(route *gin.RouterGroup, validateTokenHandle, authHandle gin.HandlerFunc) {
+	user := route.Group("/user", validateTokenHandle, authHandle)
+
 	user.GET("/", func(c *gin.Context) {
 		c.AbortWithStatusJSON(200, gin.H{
 			"status": "ok",
