8fadf994cf353b6a2fe5893d0900ff90bf56276e
Eren <alincakeren@gmail.com>
2022-02-14 20:22:32 +0300

    Template update

--- src/templates/template.go
+++ src/templates/template.go
@@ -1,31 +1,15 @@
 package templates
 
 import (
-	"html/template"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-var tmpl *template.Template
-
-type todo struct {
-	Item string
-	Done bool
-}
-
-type PageData struct {
-	Title string
-	Todos []todo
-}
-
 func LoginPage(context *gin.Context) {
-
 	context.HTML(http.StatusOK, "login.html", nil)
-
 }
+
 func RegisterPage(context *gin.Context) {
-
 	context.HTML(http.StatusOK, "register.html", nil)
-
 }
